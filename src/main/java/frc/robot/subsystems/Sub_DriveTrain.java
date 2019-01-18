b234a2598b58ce7a847cee3b99f2e18c104acd65
Damien - Laptop <dvfullerton@gmail.com>
2019-01-18 10:50:16 -0500

    Added lots of drive logic
    updated and moved last years drive logic

--- src/main/java/frc/robot/subsystems/Sub_DriveTrain.java
+++ src/main/java/frc/robot/subsystems/Sub_DriveTrain.java
@@ -17,6 +17,7 @@ import edu.wpi.first.wpilibj.SpeedControllerGroup;
 import edu.wpi.first.wpilibj.SPI.Port;
 import edu.wpi.first.wpilibj.command.Subsystem;
 import edu.wpi.first.wpilibj.drive.DifferentialDrive;
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import frc.robot.RobotMap;
 import frc.robot.commands.Cmd_ManualDrive;
 
@@ -123,16 +124,19 @@ public Sub_DriveTrain(){
   }
 
   //Auton Methods
-
-
+  //Checks for if robot is done driving to position
 
   public boolean isDoneDriving() {
-    return ((Math.abs(this.getRightSideSensorPosInTicks()) - driveSetpoint) >= 0);
+    return ((Math.abs(this.rightPostion() - driveSetpoint) >= 0));
   }
-    
+  
+  //Checks for if robot is done turning
+
   public boolean isDoneTurning(double angle) {
     return (Math.abs(angle - this.getGyroAngle()) < 2);
   }
+
+  //Method for driving to position in auton
 
   public void driveToPos( double upperSpeed, double lowerSpeed) {
     	
@@ -143,14 +147,66 @@ public Sub_DriveTrain(){
     diffDriveGroup.arcadeDrive(linearRamp(upperSpeed,lowerSpeed) * sign, 0 + offset);
     
   }
-  private double linearRamp( double upperSpeed, double lowerSpeed) {
-    double diff = (driveSetpoint - (double)Math.abs(getRightSideSensorPosInTicks()));
+
+  //Method for a linear ramping to a lower speed for auton driving
+  //This increases accuracy for auton driving
+  //uppperSpped is the fastest the robot will drive and lowerSpeed is the slowest the robot will drive
+  //The speed is measured from 0 to 1, so 0.5 will be 50% motor output and 1 will be 100%
+
+  private double linearRamp(double upperSpeed, double lowerSpeed) {
+    double diff = (driveSetpoint - (double)Math.abs(rightPostion()));
     double corrected = .05 * diff;
     double upperBound = Math.min(upperSpeed , corrected);
     double lowerBound = Math.max(lowerSpeed , upperBound);
     
     SmartDashboard.putNumber("correctedoutput", corrected);
     return lowerBound;
+  }
+
+  //Methods for limelight tracking
+
+  public void setpointCube(double areaSetpoint) {
+    CUBE_AREA_SETPOINT = areaSetpoint;
+  }
+
+  //Automatic allignment and driving method
+
+  public void trackCube(double xOffset, double areaOffset) {
+  
+  double xDiff = 0 - xOffset *-1;
+  double aDiff = 5 - areaOffset;
+  double xCorrect = 0.05 * xDiff;
+  double aCorrect = 0.4 * aDiff;
+  
+  diffDriveGroup.arcadeDrive(aCorrect, xCorrect);
+}
+
+//Automatic allighnment but accepts the left joystick Y value as speed
+//TO DO TEST THIS ONE :)
+
+public void trackCubeManualSpeed(double xOffset, Joystick joy) {
+  
+  double xDiff = 0 - xOffset *-1;
+  double xCorrect = 0.05 * xDiff;
+  
+  diffDriveGroup.arcadeDrive(joy.getY(), xCorrect);
+}
+
+//Assorted PID math for auto driving
+  
+  private double getGainP(double setpoint, double current, double kP) {
+    	
+    double error = setpoint - current;  		
+    return KP_SIMPLE * error;
+  }
+  
+  private double getGainPI(double setpoint, double current,double kP, double kI) {
+    
+    double error = setpoint - current; 
+    double p = KP_SIMPLE * error;
+    accumError = accumError + error;
+    double i = KI_SIMPLE * error;
+    return p + i;
   }
 
 
