544339242d94ac293ab0b1d2b5789beb952c28d6
Damien - Laptop <dvfullerton@gmail.com>
2019-01-28 13:18:57 -0500

    Changed out Sparks with talons
    also added new get positions and resets

--- src/main/java/frc/robot/commands/Cmd_AutoDrive.java
+++ src/main/java/frc/robot/commands/Cmd_AutoDrive.java
@@ -57,11 +57,15 @@ public class Cmd_AutoDrive extends Command {
   // Called once after isFinished returns true
   @Override
   protected void end() {
+    Robot.s_drivetrain.driveStop();
+
   }
 
   // Called when another command which requires one or more of the same
   // subsystems is scheduled to run
   @Override
   protected void interrupted() {
+    Robot.s_drivetrain.driveStop();
+
   }
 }
--- src/main/java/frc/robot/subsystems/Sub_Arm.java
+++ src/main/java/frc/robot/subsystems/Sub_Arm.java
@@ -7,8 +7,9 @@
 
 package frc.robot.subsystems;
 
-import com.revrobotics.CANSparkMax;
-import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX;
+import com.ctre.phoenix.motorcontrol.FeedbackDevice;
+
 
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.SpeedController;
@@ -24,8 +25,8 @@ public class Sub_Arm extends Subsystem {
   // Put methods for controlling this subsystem
   // here. Call these from Commands.
 
-  CANSparkMax dartMotor0 = new CANSparkMax(RobotMap.ARM_DART0, MotorType.kBrushless);
-  CANSparkMax dartMotor1 = new CANSparkMax(RobotMap.ARM_DART1, MotorType.kBrushless);
+  WPI_TalonSRX dartMotor0 = new WPI_TalonSRX(RobotMap.ARM_DART0);
+  WPI_TalonSRX dartMotor1 = new WPI_TalonSRX(RobotMap.ARM_DART1);
   DigitalInput armHomeSensor = new DigitalInput(RobotMap.ARM_HOME);
 
   private SpeedController gDartDrive = new SpeedControllerGroup(dartMotor0, dartMotor1);
@@ -36,19 +37,27 @@ public class Sub_Arm extends Subsystem {
 	private final double KP_SIMPLE = 0.05;
   private final double KI_SIMPLE = 0.03;
 
-  public double armSetpoint0 = 0;
-  public double armSetpoint1 = 0;
-  public double fakePosition;
+  public Sub_Arm(){
+
+    dartMotor0.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 10);
+    dartMotor1.configSelectedFeedbackSensor(FeedbackDevice.QuadEncoder, 0, 10);
+  }
 
 
-  private final double DRIVE_TICKS = 0.448286;
+  // TODO: Figure out the math of this
+  private final double DRIVE_TICKS = 0;
 
   public double dartMotor0Position(){
-    return dartMotor0.getEncoder().getPosition();
+    return dartMotor0.getSelectedSensorPosition();
   }
   public double dartMotor1Position(){
-    return dartMotor1.getEncoder().getPosition();
+    return dartMotor1.getSelectedSensorPosition();
   }
+public void resetArmPos(){
+  dartMotor0.setSelectedSensorPosition(0);
+  dartMotor1.setSelectedSensorPosition(0);
+
+}
 
   public boolean isArmHome(){
     return armHomeSensor.get();
@@ -56,10 +65,8 @@ public class Sub_Arm extends Subsystem {
   
 
   public void setArmPos(int distance){
-    armSetpoint0 = DRIVE_TICKS * (distance);
-    armSetpoint1 = DRIVE_TICKS * (distance);
-
-
+    double armSetpoint0 = DRIVE_TICKS * (distance);
+    double armSetpoint1 = DRIVE_TICKS * (distance);
   }
 
 
