a27f2e12b0bba542fbf9d206a5b84b58affa3b8f
Rodney <rodneyll@umich.edu>
2019-02-13 01:13:21 -0500

    Collector 'home' commands

--- src/main/java/frc/robot/commands/collector/CmdI_CollectorDropIntakeHome.java
+++ src/main/java/frc/robot/commands/collector/CmdI_CollectorDropIntakeHome.java
@@ -7,41 +7,20 @@
 
 package frc.robot.commands.collector;
 
-import edu.wpi.first.wpilibj.command.Command;
+import edu.wpi.first.wpilibj.command.InstantCommand;
 import frc.robot.Robot;
 
-public class Cmd_CollectorCollectCargo extends Command {
-  public Cmd_CollectorCollectCargo() {
+public class CmdI_CollectorDropIntakeHome extends InstantCommand {
+ 
+  public CmdI_CollectorDropIntakeHome() {
+    super();
     requires(Robot.s_collector);
   }
 
   @Override
   protected void initialize() {
-    Robot.s_collector.extendDropMotors();
-    Robot.s_collector.armIntakeCollect();
-    Robot.s_collector.dropMotorSetSpeed(1);
-  }
-
-  @Override
-  protected void execute() {
-    //No need to run continuously 
-  }
-
-  @Override
-  protected boolean isFinished() {
-    return(Robot.s_collector.isBallSensor() == false);
-  }
-
-  @Override
-  protected void end() {
     Robot.s_collector.dropMotorSetSpeed(0);
-    Robot.s_collector.armIntakeStop();
     Robot.s_collector.retractDropMotors();
   }
 
-  @Override
-  protected void interrupted() {
-    //Interrupt same as end
-    end();
-  }
 }
