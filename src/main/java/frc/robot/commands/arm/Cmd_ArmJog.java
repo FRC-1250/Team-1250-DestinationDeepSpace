4d465c178d475499aef2a580d8331e9edef51cbe
NotDriver <32750071+RincewindBoi@users.noreply.github.com>
2019-02-15 19:59:13 -0500

    Go to pos and pid values changed

--- src/main/java/frc/robot/commands/arm/Cmd_ArmJog.java
+++ src/main/java/frc/robot/commands/arm/Cmd_ArmJog.java
@@ -28,7 +28,7 @@ public class Cmd_ArmJog extends Command {
     float sign = Math.signum((float)signvalue);
 
     if((int)Robot.m_oi.getLeftField().getRawAxis(0) < 0){
-      if(sign == -1 || Robot.s_arm.isArmHome() == false){
+      if( Robot.s_arm.isArmHome() == false){
         Robot.s_arm.armStop();
       }
       else{
